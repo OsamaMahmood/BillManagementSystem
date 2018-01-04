fe2790e34b49cc70dad81c2e519ce84022d2289e
Osama Mahmood <osama.mahmood40@gmail.com>
2018-01-04 14:04:35 +0500

    Fields for Admin Table

---
+++ src/Models/Admin.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author m4rkm3n
+ */
+public class Admin {
+    private int adminID;
+    private String aname;
+    private String aemail;
+    private String apassword;
+
+    public int getAdminID() {
+        return adminID;
+    }
+
+    public void setAdminID(int adminID) {
+        this.adminID = adminID;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public String getAemail() {
+        return aemail;
+    }
+
+    public void setAemail(String aemail) {
+        this.aemail = aemail;
+    }
+
+    public String getApassword() {
+        return apassword;
+    }
+
+    public void setApassword(String apassword) {
+        this.apassword = apassword;
+    }
+    
+    
+    
+}
